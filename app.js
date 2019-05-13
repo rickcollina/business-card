c6d30eabc4fd8d1e27b8ea5c199c75b805fd4c8a
Ricardo Coline Arcega <ricardoarcega@Rick-Arcega-MACBOOK-PRO.local>
2019-05-12 23:32:12 -0300

    Changing the Template

--- app.js
+++ app.js
@@ -1,7 +1,87 @@
+
+
+
+
+
 const express = require('express')
-const app = express()
-const port = 3000
+const server = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+/* other routes defined before catch-all */
+server.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+/* final catch-all route to index.html defined last */
+server.get('/vendor/bootstrap/css/bootstrap.min.css', (req, res) => {
+  res.sendFile(__dirname + '/vendor/bootstrap/css/bootstrap.min.css');
+})
+
+server.get('/vendor/fontawesome-free/css/all.min.css', (req, res) => {
+  res.sendFile(__dirname + '/vendor/fontawesome-free/css/all.min.css');
+})
+
+server.get('/vendor/simple-line-icons/css/simple-line-icons.css', (req, res) => {
+  res.sendFile(__dirname + '/vendor/simple-line-icons/css/simple-line-icons.css');
+})
+
+server.get('/css/landing-page.min.css', (req, res) => {
+  res.sendFile(__dirname + '/css/landing-page.min.css');
+})
+
+server.get('/vendor/jquery/jquery.min.js', (req, res) => {
+  res.sendFile(__dirname + '/vendor/jquery/jquery.min.js');
+})
+
+server.get('/vendor/bootstrap/js/bootstrap.bundle.min.js', (req, res) => {
+  res.sendFile(__dirname + '/vendor/bootstrap/js/bootstrap.bundle.min.js');
+})
+
+server.get('/img/RicardoFoto.jpg', (req, res) => {
+  res.sendFile(__dirname + '/img/RicardoFoto.jpg');
+})
+
+
+
+server.get('/vendor/fontawesome-free/webfonts/fa-brands-400.ttf', (req, res) => {
+  res.sendFile(__dirname + '/vendor/fontawesome-free/webfonts/fa-brands-400.ttf');
+})
+
+server.get('/vendor/fontawesome-free/webfonts/fa-brands-400.woff', (req, res) => {
+  res.sendFile(__dirname + '/vendor/fontawesome-free/webfonts/fa-brands-400.woff');
+})
+
+server.get('/vendor/fontawesome-free/webfonts/fa-brands-400.woff2', (req, res) => {
+  res.sendFile(__dirname + '/vendor/fontawesome-free/webfonts/fa-brands-400.woff2');
+})
+
+server.get('/vendor/fontawesome-free/webfonts/fa-regular-400.ttf', (req, res) => {
+  res.sendFile(__dirname + '/vendor/fontawesome-free/webfonts/fa-regular-400.ttf');
+})
+
+server.get('/vendor/fontawesome-free/webfonts/fa-regular-400.woff2', (req, res) => {
+  res.sendFile(__dirname + '/vendor/fontawesome-free/webfonts/fa-regular-400.woff2');
+})
+
+server.get('/vendor/fontawesome-free/webfonts/fa-regular-400.woff', (req, res) => {
+  res.sendFile(__dirname + '/vendor/fontawesome-free/webfonts/fa-regular-400.woff');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port = 8000;
+server.listen(port, function() {
+  console.log('server listening on port ' + port)
+})
